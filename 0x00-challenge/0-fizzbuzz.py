13aebb2b033fe0a772e0ae63a737bedb4e6bbda0
ben <ferdinandkoli09@gmail.com>
2022-12-01 16:27:31 +0100

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+
 """ FizzBuzz
-    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
+
 import sys
 
 
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-    
+
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -33,8 +34,13 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+        print("Example: ./0-fizzbuzz.py 89")                                                                        
+        sys.exit(1)                                                                                                 
+    try:                                                                                                            
+        number = int(sys.argv[1])                                                                                   
+        fizzbuzz(number)                                                                                            
+    except ValueError:                                                                                              
+        print("Input is not a number")                                                                              
+        print("Usage: ./0-fizzbuzz.py <number>")                                                                    
+        print("Example: ./0-fizzbuzz.py 89")                                                                        
+        sys.exit(1) 
